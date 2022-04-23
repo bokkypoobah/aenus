083d33c7f13c037f3aa5efee3bb96883488f6621
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2022-04-23 11:11:02 +1000

    wip

--- docs/routes.js
+++ docs/routes.js
@@ -6,33 +6,33 @@ const routes = [{
     path: '/portfolio',
     component: Portfolio,
     name: 'Portfolio',
-  }, {
-    path: '/admin',
-    component: Admin,
-    name: 'Admin',
-  }, {
-    path: '/weth',
-    component: WETH,
-    name: 'WETH',
-  }, {
-    path: '/exchange',
-    component: Exchange,
-    name: 'Exchange',
-  }, {
-    path: '/collections',
-    component: Collections,
-    name: 'Collections',
-  }, {
-    path: '/docs/:section/:topic',
-    component: Docs,
-    name: 'Docs',
-  }, {
-    path: '/:account/:collection',
-    component: Collections,
-    name: '',
+  // }, {
+  //   path: '/admin',
+  //   component: Admin,
+  //   name: 'Admin',
+  // }, {
+  //   path: '/weth',
+  //   component: WETH,
+  //   name: 'WETH',
+  // }, {
+  //   path: '/exchange',
+  //   component: Exchange,
+  //   name: 'Exchange',
+  // }, {
+  //   path: '/collections',
+  //   component: Collections,
+  //   name: 'Collections',
+  // }, {
+  //   path: '/docs/:section/:topic',
+  //   component: Docs,
+  //   name: 'Docs',
+  // }, {
+  //   path: '/:account/:collection',
+  //   component: Collections,
+  //   name: '',
   }, {
     path: '*',
-    component: Welcome,
+    component: Portfolio,
     name: ''
   }
 ];
