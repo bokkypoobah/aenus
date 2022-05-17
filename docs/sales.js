00371bcbefea9fbcc6c1b0be6b6b576489bf252d
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2022-05-17 18:15:10 +1000

    sales wip

--- docs/sales.js
+++ docs/sales.js
@@ -12,10 +12,12 @@ const Sales = {
         <b-card no-body class="p-0 mt-1">
 
           <b-card-body class="m-1 p-1">
-            <b-button size="sm" @click="doit( { type: 'fullsync' } );" :disabled="searchMessage != null" variant="primary">{{ searchMessage ? searchMessage : 'Retrieve Latest 50 Sales'}}</b-button>
+            <b-button size="sm" @click="doit( { type: 'fullsync' } );" variant="primary">Retrieve Latest 50 Sales</b-button>
+            <!--
             <span v-if="searchMessage != null">
               <b-button size="sm" @click="halt" variant="primary">Halt</b-button>
             </span>
+            -->
           </b-card-body>
           <b-card-body class="m-1 p-1">
             <b-table small striped hover :fields="salesFields" :items="sales" table-class="w-auto" class="mt-1">
@@ -147,91 +149,7 @@ const Sales = {
         resultsTabIndex: 0,
       },
 
-      searchOptions: [
-        { value: 'exact', text: 'Exact Name' },
-        { value: 'contains', text: 'Name Contains' },
-        { value: 'startswith', text: 'Name Starts With' },
-        { value: 'endswith', text: 'Name Ends With' },
-      ],
-
-      pageSizes: [
-        { value: 10, text: '10/P' },
-        { value: 100, text: '100/P' },
-        { value: 500, text: '500/P' },
-        { value: 1000, text: '1,000/P' },
-        { value: 2145, text: '2,145/P' },
-        { value: 66666, text: 'ALL' },
-      ],
-
-      setOptions: [
-        { value: 'digit9', text: 'Digits 0 to 9 [prefix/postfix required for min 3 length]' },
-        { value: 'digit99', text: 'Digits 00 to 99, [prefix/postfix required for min 3 length]' },
-        { value: 'digit999', text: 'Digits 000 to 999 [Club999]' },
-        { value: 'digit9999', text: 'Digits 0000 to 9999 [Club10k]' },
-        { value: 'digit99999', text: 'Digits 00000 to 99999 [Club100k]' },
-        { value: 'digit999999', text: 'Digits 000000 to 999999' },
-        { value: 'digit9999999', text: 'Digits 0000000 to 9999999' },
-        { value: 'digit99999999', text: 'Digits 00000000 to 99999999' },
-        { value: 'hours', text: 'Hours 00h00 to 23h59' },
-      ],
-
-      imageSizeOptions: [
-        { value: '93', text: '93%' },
-        { value: '115', text: '115%' },
-        { value: '240', text: '240%' },
-        { value: '300', text: '300%' },
-        { value: '500', text: '500%' },
-        { value: '750', text: '750%' },
-        { value: '1000', text: '1,000%' },
-        { value: '1500', text: '1,500%' },
-        { value: '3000', text: '3,000%' },
-        { value: '6000', text: '6,000%' },
-        { value: '12000', text: '12,000%' },
-      ],
-
       results: [],
-
-      sortOptions: [
-        { value: 'nameasc', text: 'Name Ascending' },
-        { value: 'namedsc', text: 'Name Descending' },
-        { value: 'priceasc', text: 'Price Ascending' },
-        { value: 'pricedsc', text: 'Price Descending' },
-        { value: 'expiryasc', text: 'Expiry Ascending' },
-        { value: 'expirydsc', text: 'Expiry Descending' },
-        { value: 'registrationasc', text: 'Registration Ascending' },
-        { value: 'registrationdsc', text: 'Registration Descending' },
-        { value: 'lengthname', text: 'Length Ascending, Name Ascending' },
-        { value: 'random', text: 'Random' },
-      ],
-
-      resultsFields: [
-        { key: 'index', label: '#', thStyle: 'width: 5%;' },
-        { key: 'image', label: 'Image', thStyle: 'width: 10%;', sortable: false },
-        { key: 'name', label: 'Name', thStyle: 'width: 30%;', sortable: false },
-        { key: 'registrant', label: 'Registrant/Controller/Resolved Address', thStyle: 'width: 35%;', sortable: false },
-        { key: 'expiryDate', label: 'Expiry/Registration (UTC)', thStyle: 'width: 20%;', sortable: false },
-      ],
-      summaryFields: [
-        { key: 'names', label: 'Names' },
-      ],
-      ownersFields: [
-        { key: 'index', label: '#', thStyle: 'width: 5%;' },
-        { key: 'registrant', label: 'Registrant', thStyle: 'width: 30%;' },
-        { key: 'length', label: '#Names', thStyle: 'width: 5%;' },
-        { key: 'names', label: 'Names', thStyle: 'width: 60%;' },
-      ],
-
-      // saleRecords.push({
-      //   chainId: chainId,
-      //   contract: ENSADDRESS,
-      //   tokenId: sale.token.tokenId,
-      //   name: name,
-      //   from: sale.from,
-      //   to: sale.to,
-      //   price: sale.price,
-      //   timestamp: sale.timestamp,
-      //   // data: sale,
-      // });
 
       salesFields: [
         { key: 'timestamp', label: 'Timestamp', thStyle: 'width: 20%;' },
@@ -256,143 +174,8 @@ const Sales = {
     network() {
       return store.getters['connection/network'];
     },
-    searchResults() {
-      return store.getters['search/results'];
-    },
-    unregistered() {
-      return store.getters['search/unregistered'];
-    },
-    searchMessage() {
-      return store.getters['search/message'];
-    },
-    groups() {
-      return store.getters['config/groups'];
-    },
-    prices() {
-      return store.getters['search/prices'];
-    },
     sales() {
       return store.getters['sales/sales'];
-    },
-
-    groupOptions() {
-      const results = [];
-      if (this.groups) {
-        if (this.coinbase) {
-          results.push({ value: null, text: "Current account (" + this.coinbase + ")" });
-        } else {
-          results.push({ value: null, text: "Connect your web3 wallet & create your groups in Config" });
-        }
-        let i = 0;
-        for (const group of this.groups) {
-          results.push({ value: i++, text: group.name });
-        }
-      }
-      return results;
-    },
-    filteredResults() {
-      const results = this.settings.randomise ? [] : [];
-      const regex = this.settings.filter != null && this.settings.filter.length > 0 ? new RegExp(this.settings.filter, 'i') : null;
-      const priceFrom = this.settings.priceFrom && parseFloat(this.settings.priceFrom) > 0 ? parseFloat(this.settings.priceFrom) : null;
-      const priceTo = this.settings.priceTo && parseFloat(this.settings.priceTo) > 0 ? parseFloat(this.settings.priceTo) : null;
-
-      if (regex == null && priceFrom == null && priceTo == 0) {
-        for (result of Object.values(this.searchResults)) {
-          results.push(result);
-        }
-      } else {
-        for (result of Object.values(this.searchResults)) {
-          let include = true;
-          if (regex && !regex.test(result.labelName)) {
-            include = false;
-          }
-          if (include && (priceFrom || priceTo)) {
-            const price = this.prices[result.tokenId] && this.prices[result.tokenId].floorAskPrice;
-            if (price) {
-              if (priceFrom && price < priceFrom) {
-                include = false;
-              } else if (priceTo && price > priceTo) {
-                include = false;
-              }
-            } else {
-              include = false;
-            }
-          }
-          if (include) {
-            results.push(result);
-          }
-        }
-      }
-
-      if (this.settings.sortOption == 'nameasc') {
-        results.sort(function (a, b) {
-            return ('' + a.labelName).localeCompare(b.labelName);
-        })
-      } else if (this.settings.sortOption == 'namedsc') {
-        results.sort(function (a, b) {
-            return ('' + b.labelName).localeCompare(a.labelName);
-        })
-      } else if (this.settings.sortOption == 'priceasc') {
-        results.sort((a, b) => {
-          const pricea = this.prices[a.tokenId] ? this.prices[a.tokenId].floorAskPrice : null;
-          const priceb = this.prices[b.tokenId] ? this.prices[b.tokenId].floorAskPrice : null;
-          if (pricea == priceb) {
-            return ('' + a.labelName).localeCompare(b.labelName);
-          } else if (pricea != null && priceb == null) {
-            return -1;
-          } else if (pricea == null && priceb != null) {
-            return 1;
-          } else {
-            return pricea - priceb;
-          }
-        });
-      } else if (this.settings.sortOption == 'pricedsc') {
-        results.sort((a, b) => {
-          const pricea = this.prices[a.tokenId] ? this.prices[a.tokenId].floorAskPrice : null;
-          const priceb = this.prices[b.tokenId] ? this.prices[b.tokenId].floorAskPrice : null;
-          if (pricea == priceb) {
-            return ('' + a.labelName).localeCompare(b.labelName);
-          } else if (pricea != null && priceb == null) {
-            return -1;
-          } else if (pricea == null && priceb != null) {
-            return 1;
-          } else {
-            return priceb - pricea;
-          }
-        });
-      } else if (this.settings.sortOption == 'expiryasc') {
-        results.sort((a, b) => {
-          return a.expiryDate - b.expiryDate;
-        });
-      } else if (this.settings.sortOption == 'expirydsc') {
-        results.sort((a, b) => {
-          return b.expiryDate - a.expiryDate;
-        });
-      } else if (this.settings.sortOption == 'registrationasc') {
-        results.sort((a, b) => {
-          return a.registrationDate - b.registrationDate;
-        });
-      } else if (this.settings.sortOption == 'registrationdsc') {
-        results.sort((a, b) => {
-          return b.registrationDate - a.registrationDate;
-        });
-      } else if (this.settings.sortOption == 'lengthname') {
-        results.sort((a, b) => {
-          if (a.length == b.length) {
-            return ('' + a.labelName).localeCompare(b.labelName);
-          } else {
-            return a.length - b.length;
-          }
-        });
-      } else {
-        results.sort(() => {
-          return Math.random() - 0.5;
-        });
-      }
-      return results;
-    },
-    pagedFilteredResults() {
-      return this.filteredResults.slice((this.settings.resultsCurrentPage - 1) * this.settings.resultsPageSize, this.settings.resultsCurrentPage * this.settings.resultsPageSize);
     },
   },
   methods: {
@@ -403,48 +186,16 @@ const Sales = {
       }
       return e.toFixed(9);
     },
-    formatBuyOrSell(buyOrSell) {
-      return BUYORSELLSTRING[buyOrSell];
-    },
-    formatAnyOrAll(anyOrAll) {
-      return ANYORALLSTRING[anyOrAll];
-    },
-    formatOrderStatus(orderStatus) {
-      return ORDERSTATUSSTRING[orderStatus];
-    },
-    formatDate(d) {
-      if (d == 0) {
-        return "(no expiry)";
-      } else {
-        if (new RegExp('^[0-9]+$').test(d)) {
-          return new Date(parseInt(d) * 1000).toISOString().replace('T', ' ').replace('.000Z', ''); // .substring(4);
-        } else {
-          return new Date(d).toDateString().substring(4);
-        }
-      }
-    },
     formatTimestamp(ts) {
       return new Date(ts * 1000).toLocaleString();
     },
-
-    setPowerOn() {
-      store.dispatch('connection/setPowerOn', true);
-      localStorage.setItem('powerOn', true);
-      var t = this;
-      setTimeout(function() {
-        t.statusSidebar = true;
-      }, 1500);
-    },
-
     async doit(options) {
       console.log("doit: " + JSON.stringify(options));
       store.dispatch('sales/doit', options);
     },
-
     async halt() {
       store.dispatch('search/halt');
     },
-
     async timeoutCallback() {
       logDebug("Sales", "timeoutCallback() count: " + this.count);
 
