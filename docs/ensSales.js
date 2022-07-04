06dcee9ad45a3d0570968d23e0472cfc7dafba38
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2022-07-04 11:03:03 +1000

    ens sales filter chinese numerals

--- docs/ensSales.js
+++ docs/ensSales.js
@@ -322,6 +322,7 @@ const ENSSales = {
             { value: '^[\u0660-\u0669]*$', text: 'Arabic Numerals - ٠ to ٩' },
             { value: '^[\u09E6-\u09EF]*$', text: 'Bengali Numerals - ০, ১, ২, ৩, ৪, ৫, ৬, ৭, ৮, ৯' },
             { value: '^[영일이삼사오육칠팔구]*$', text: 'Sino-Korean Numerals - 영, 일, 이, 삼, 사, 오, 육, 칠, 팔, 구' },
+            { value: '^[〇一二三四五六七八九十百]*$', text: 'Chinese Numerals - 〇, 一, 二, 三, 四, 五, 六, 七, 八, 九, 十, 百' },
             { value: '^[\u0E50-\u0E59]*$', text: 'Thai Numerals - ๐ to ๙' },
           ],
         },
