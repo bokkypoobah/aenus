7a1c45cb7dcc576a1191d7e3139e51a6206be074
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2022-07-06 10:07:59 +1000

    tidy

--- docs/accounts.js
+++ docs/accounts.js
@@ -304,7 +304,7 @@ const Accounts = {
                   </template>
                   <template #cell(txHash)="data">
                     <b-link :href="'https://etherscan.io/tx/' + data.item.txHash" v-b-popover.hover="'View in etherscan.io'" target="_blank">
-                      {{ data.item.txHash.substring(0, 6) }}
+                      {{ data.item.txHash.substring(0, 10) }}
                     </b-link>
                     <font v-if="data.item.txHash in transactions" size="-1">
                       <b-badge variant="light" v-b-popover.hover.bottom="JSON.stringify(transactions[data.item.txHash].tx, null, 2)">tx</b-badge>
